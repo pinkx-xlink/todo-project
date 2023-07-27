e39a76b51f56e39295fc822eaf08620681c48c97
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-07-27 17:37:59 -0400

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-// const path = require('path')
+ const path = require('path')
 
-// module.exports = {
-//     entry: './src/index.js',
-//     output: {
-//         filename: 'index.js',
-//         path: path.resolve(__dirname, 'dist'),
+ module.exports = {
+     entry: './src/index.js',
+     output: {
+         filename: 'index.js',
+         path: path.resolve(__dirname, 'dist'),
 //         //not sure yet what name to change to...^
-//     },
-// };
+    },
+ };
