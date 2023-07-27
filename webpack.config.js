ff7860837001844770e2df960fe8e5a98582cb6a
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-07-27 14:42:43 -0400

    Edit line 7 - remove ./ from dist file

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: {
         main: './src/index.js',
     output: {
-        path: path.resolve(__dirname, './dist'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'src.bundle.js',
         //not sure yet what name to change to...^
     },
