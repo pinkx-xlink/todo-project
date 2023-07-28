63684a10fa39d3911e4c65a33242a2c7abbc355b
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-07-28 10:12:05 -0400

    Delete webpack-demo js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-import _ from 'lodash';
-
-function component() {
-    const element = document.createElement('div');
-
-    //lodash required for this
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    return element;
-}
-
-document.body.appendChild(component());
 
 // function addTask() {
 //     let taskElement = document.querySelector("task");
