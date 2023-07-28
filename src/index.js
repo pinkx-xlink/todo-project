25bb5608f0c8acf4e49daeb3adc5deceb2732a9f
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-07-28 10:15:48 -0400

    Add some basic JS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const tasks_container = document.getElementById("tasks");
 const task_template = document.getElementById("taskTemplate");
-const add_button = document.getElementById("takTemplate");
+const add_button = document.getElementById("add");
+
+function getTasks() {
+    
+}
 
 
 
