1558a1caad81355ed08c930aca9c37cb23ac46b4
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-07-28 10:14:04 -0400

    Change todo div class > tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+const tasks_container = document.getElementById("tasks");
+const task_template = document.getElementById("taskTemplate");
+const add_button = document.getElementById("takTemplate");
+
+
+
 
 // function addTask() {
 //     let taskElement = document.querySelector("task");
