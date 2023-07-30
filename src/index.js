107f747b4c9d6b93286dd18d5c3f0af65acf9701
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-07-30 16:01:51 -0400

    Add functions for setTasks and addTask

--- src/index.js
+++ src/index.js
@@ -2,14 +2,30 @@ const tasks_container = document.getElementById("tasks");
 const task_template = document.getElementById("taskTemplate");
 const add_button = document.getElementById("add");
 
+let Tasks = getTasks();
+
 function getTasks() {
   const value = localStorage.getTask("todo-test") || "[]";
   
   return JSON.parse(value);
 }
 
+function setTasks(tasks) {
+    const tasksJSON.stringify(tasks);
 
-getTasks();
+    localStorage.setTask("todo-test", tasksJson);
+
+
+}
+
+function addTask() {
+    tasks.unshift({
+        description: "",
+        completed: false
+    });
+}
+
+console.log(tasks);
 
 // function addTask() {
 //     let taskElement = document.querySelector("task");
