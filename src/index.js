335c00b8d2b9b4aeee796c957a68e10d356d061a
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-07-30 15:56:20 -0400

    Add return JSON.parse to line 8

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const task_template = document.getElementById("taskTemplate");
 const add_button = document.getElementById("add");
 
 function getTasks() {
-  const value = localStorage.getTask("todo");
+  const value = localStorage.getTask("todo-test") || "[]";
   
-  console.log(value);
+  return JSON.parse(value);
 }
 
 
