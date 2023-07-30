d5128aa9563221527b4895b08451d6ac616e636e
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-07-30 15:54:48 -0400

    Create getTasks function

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const task_template = document.getElementById("taskTemplate");
 const add_button = document.getElementById("add");
 
 function getTasks() {
-    
+  const value = localStorage.getTask("todo");
+  
+  console.log(value);
 }
 
 
-
+getTasks();
 
 // function addTask() {
 //     let taskElement = document.querySelector("task");
